cca61719c2a468d8ef370f1c071f7630b5a5af82
mmdreza <m13r81a@gmail.come>
2023-02-01 23:48:52 +0330

    style of destinations

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,19 @@
 const navBtn = document.querySelector(".nav__btn")
 const navMenu = document.querySelector(".nav-menu")
+const placeLikeIcon = document.querySelectorAll('.place__like-icon') 
 navBtn.addEventListener("click", function(){
     navBtn.classList.toggle("nav__btn--open")
     navMenu.classList.toggle("nav-menu--open")
     
 })
 
+
+
+// === add to favorite ===
+placeLikeIcon.forEach(function(heartIcon) {
+  heartIcon.addEventListener('click', function(event) {
+    heartIcon.classList.toggle('place__like-icon--fill')
+
+   
+  });
+});
